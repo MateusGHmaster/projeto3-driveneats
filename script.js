ac95d4339dd676af21f79c64ef3d2b6ae4d12f44
Mateus <mateusoliveira469@gmail.com>
2022-01-31 15:19:51 -0400

    button 1.0

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let plateSelected;
+let drinkSelected;
+let dessertSelected;
+
+
 //plate-selection-border
 
 function itemPlate1() {
@@ -5,6 +10,7 @@ function itemPlate1() {
     document.getElementById("plate2").style.borderColor="white";
     document.getElementById("plate3").style.borderColor="white";
     document.getElementById("plate4").style.borderColor="white";
+    plateSelected = "Bisteca de Suína Frita";
 }
 
 function itemPlate2() {
@@ -12,6 +18,7 @@ function itemPlate2() {
     document.getElementById("plate1").style.borderColor="white";
     document.getElementById("plate3").style.borderColor="white";
     document.getElementById("plate4").style.borderColor="white";
+    plateSelected = "Bist.Suína Grelhada";
 }
 
 function itemPlate3() {
@@ -19,6 +26,7 @@ function itemPlate3() {
     document.getElementById("plate1").style.borderColor="white";
     document.getElementById("plate2").style.borderColor="white";
     document.getElementById("plate4").style.borderColor="white";
+    plateSelected = "Bife Brasileiro";
 }
 
 function itemPlate4() {
@@ -26,6 +34,7 @@ function itemPlate4() {
     document.getElementById("plate1").style.borderColor="white";
     document.getElementById("plate2").style.borderColor="white";
     document.getElementById("plate3").style.borderColor="white";
+    plateSelected = "Bifa a Cavalo";
 }
 
 
@@ -37,6 +46,7 @@ function itemDrink1() {
     document.getElementById("drink2").style.borderColor="white";
     document.getElementById("drink3").style.borderColor="white";
     document.getElementById("drink4").style.borderColor="white";
+    drinkSelected = "Coca-Cola";
 }
 
 function itemDrink2() {
@@ -44,6 +54,7 @@ function itemDrink2() {
     document.getElementById("drink1").style.borderColor="white";
     document.getElementById("drink3").style.borderColor="white";
     document.getElementById("drink4").style.borderColor="white";
+    drinkSelected = "Fanta Laranja";
 }
 
 function itemDrink3() {
@@ -51,6 +62,7 @@ function itemDrink3() {
     document.getElementById("drink1").style.borderColor="white";
     document.getElementById("drink2").style.borderColor="white";
     document.getElementById("drink4").style.borderColor="white";
+    drinkSelected = "Pepsi";
 }
 
 function itemDrink4() {
@@ -58,6 +70,7 @@ function itemDrink4() {
     document.getElementById("drink1").style.borderColor="white";
     document.getElementById("drink2").style.borderColor="white";
     document.getElementById("drink3").style.borderColor="white";
+    drinkSelected = "Água com gás";
 }
 
 //dessert-selection-border
@@ -67,6 +80,7 @@ function itemDessert1() {
     document.getElementById("dessert2").style.borderColor="white";
     document.getElementById("dessert3").style.borderColor="white";
     document.getElementById("dessert4").style.borderColor="white";
+    dessertSelected = "Bolo de chocolate";
 }
 
 function itemDessert2() {
@@ -74,6 +88,7 @@ function itemDessert2() {
     document.getElementById("dessert").style.borderColor="white";
     document.getElementById("dessert").style.borderColor="white";
     document.getElementById("dessert").style.borderColor="white";
+    dessertSelected = "Bolo de frutas";
 }
 
 function itemDessert3() {
@@ -81,6 +96,7 @@ function itemDessert3() {
     document.getElementById("dessert1").style.borderColor="white";
     document.getElementById("dessert2").style.borderColor="white";
     document.getElementById("dessert4").style.borderColor="white";
+    dessertSelected = "Picolé de fruta";
 }
 
 function itemDessert4() {
@@ -88,4 +104,14 @@ function itemDessert4() {
     document.getElementById("dessert1").style.borderColor="white";
     document.getElementById("dessert2").style.borderColor="white";
     document.getElementById("dessert3").style.borderColor="white";
+    dessertSelected = "Sorvete 1 bola";
+}
+
+//turn-button-available
+
+function closeOrder() {
+    if ((plateSelected != null) && (drinkSelected != null) && (dessertSelected != null)) {
+        document.getElementById("Button").disabled = false;
+        document.getElementById("Button").style.backgroundColor = "green";
+    }
 }
