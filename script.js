80f998d969a68f47aca81319ffc917f288da50a6
Mateus <mateusoliveira469@gmail.com>
2022-01-31 13:18:58 -0400

    js 1.0

---
+++ script.js
@@ -0,0 +1,91 @@
+//plate-selection-border
+
+function itemPlate1() {
+    document.getElementById("plate1").style.borderColor="green";
+    document.getElementById("plate2").style.borderColor="white";
+    document.getElementById("plate3").style.borderColor="white";
+    document.getElementById("plate4").style.borderColor="white";
+}
+
+function itemPlate2() {
+    document.getElementById("plate2").style.borderColor="green";
+    document.getElementById("plate1").style.borderColor="white";
+    document.getElementById("plate3").style.borderColor="white";
+    document.getElementById("plate4").style.borderColor="white";
+}
+
+function itemPlate3() {
+    document.getElementById("plate3").style.borderColor="green";
+    document.getElementById("plate1").style.borderColor="white";
+    document.getElementById("plate2").style.borderColor="white";
+    document.getElementById("plate4").style.borderColor="white";
+}
+
+function itemPlate4() {
+    document.getElementById("plate4").style.borderColor="green";
+    document.getElementById("plate1").style.borderColor="white";
+    document.getElementById("plate2").style.borderColor="white";
+    document.getElementById("plate3").style.borderColor="white";
+}
+
+
+
+//drink-selection-border
+
+function itemDrink1() {
+    document.getElementById("drink1").style.borderColor="green";
+    document.getElementById("drink2").style.borderColor="white";
+    document.getElementById("drink3").style.borderColor="white";
+    document.getElementById("drink4").style.borderColor="white";
+}
+
+function itemDrink2() {
+    document.getElementById("drink2").style.borderColor="green";
+    document.getElementById("drink1").style.borderColor="white";
+    document.getElementById("drink3").style.borderColor="white";
+    document.getElementById("drink4").style.borderColor="white";
+}
+
+function itemDrink3() {
+    document.getElementById("drink3").style.borderColor="green";
+    document.getElementById("drink1").style.borderColor="white";
+    document.getElementById("drink2").style.borderColor="white";
+    document.getElementById("drink4").style.borderColor="white";
+}
+
+function itemDrink4() {
+    document.getElementById("drink4").style.borderColor="green";
+    document.getElementById("drink1").style.borderColor="white";
+    document.getElementById("drink2").style.borderColor="white";
+    document.getElementById("drink3").style.borderColor="white";
+}
+
+//dessert-selection-border
+
+function itemDessert1() {
+    document.getElementById("dessert1").style.borderColor="green";
+    document.getElementById("dessert2").style.borderColor="white";
+    document.getElementById("dessert3").style.borderColor="white";
+    document.getElementById("dessert4").style.borderColor="white";
+}
+
+function itemDessert2() {
+    document.getElementById("dessert2").style.borderColor="green";
+    document.getElementById("dessert").style.borderColor="white";
+    document.getElementById("dessert").style.borderColor="white";
+    document.getElementById("dessert").style.borderColor="white";
+}
+
+function itemDessert3() {
+    document.getElementById("dessert3").style.borderColor="green";
+    document.getElementById("dessert1").style.borderColor="white";
+    document.getElementById("dessert2").style.borderColor="white";
+    document.getElementById("dessert4").style.borderColor="white";
+}
+
+function itemDessert4() {
+    document.getElementById("dessert4").style.borderColor="green";
+    document.getElementById("dessert1").style.borderColor="white";
+    document.getElementById("dessert2").style.borderColor="white";
+    document.getElementById("dessert3").style.borderColor="white";
+}
